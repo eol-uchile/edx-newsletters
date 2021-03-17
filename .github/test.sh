060f8d8e1c6a2b5fffc08d3db0c6d982cfc391e9
matsalinas <matsalinas@uchile.cl>
2021-03-17 20:21:37 +0000

    Update tests Koa

--- .github/test.sh
+++ .github/test.sh
@@ -1,12 +1,12 @@
 #!/bin/dash
 pip install -e /openedx/requirements/edx-newsletters
 
-cd /openedx/requirements/edx-newsletters/edxnewsletters
+cd /openedx/requirements/edx-newsletters
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/edx-newsletters/edxnewsletters
+cd /openedx/requirements/edx-newsletters
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest edxnewsletters/tests.py
