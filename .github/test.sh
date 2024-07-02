0251ba4a09c840c24b6c3b787e6eff9f2a0ebf0c
fsologureng <fsologur@uchile.cl>
2024-07-02 17:00:21 -0400

    ci: centralize external workflow

--- .github/test.sh
+++ .github/test.sh
@@ -1,12 +1,13 @@
 #!/bin/dash
-pip install -e /openedx/requirements/edx-newsletters
 
-cd /openedx/requirements/edx-newsletters/edxnewsletters
+pip install -e /openedx/requirements/app
+
+cd /openedx/requirements/app
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/edx-newsletters/edxnewsletters
+cd /openedx/requirements/app
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest edxnewsletters/tests.py
